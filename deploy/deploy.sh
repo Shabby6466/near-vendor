4f2e8d2b330b6461615eb0390d9812dde47dc5da
Shoaib Fayyaz <shoaib@coinchance.io>
2026-03-26 00:45:41 +0500

    jenkins-fix

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -11,22 +11,23 @@
 #
 
 COMPOSE_FILE="docker-compose.yml"
+PROJECT_NAME="nearvendor"
 
 case "$1" in
     start)
-        docker compose -f $COMPOSE_FILE up -d
+        docker compose -f $COMPOSE_FILE -p $PROJECT_NAME up -d
         ;;
     stop)
-        docker compose -f $COMPOSE_FILE stop
+        docker compose -f $COMPOSE_FILE -p $PROJECT_NAME stop
         ;;
     down)
-        docker compose -f $COMPOSE_FILE down
+        docker compose -f $COMPOSE_FILE -p $PROJECT_NAME down
         ;;
     logs-f)
-        docker compose -f $COMPOSE_FILE logs -f
+        docker compose -f $COMPOSE_FILE -p $PROJECT_NAME logs -f
         ;;
     build)
-        docker compose -f $COMPOSE_FILE build
+        docker compose -f $COMPOSE_FILE -p $PROJECT_NAME build
         ;;
     *)
         echo "Usage: $0 {start|stop|down|logs-f|build}"
