53852b068f2f90ce0762790cd813c5767fb9e5b7
Shoaib Fayyaz <shoaib@coinchance.io>
2026-03-29 17:07:37 +0500

    fix

---
+++ src/models/entities/wishlist.entity.ts
@@ -0,0 +1,39 @@
+import { BaseEntity } from "@modules/common/entity/base.entity";
+import { Entity, Column, ManyToOne, JoinColumn, Index } from "typeorm";
+import { User } from "./users.entity";
+
+export enum WishlistStatus {
+    PENDING = 'PENDING',
+    FULFILLED = 'FULFILLED'
+}
+
+@Entity({ name: "wishlists" })
+export class Wishlist extends BaseEntity {
+    @ManyToOne(() => User, (user) => user.wishlists, { onDelete: 'CASCADE' })
+    @JoinColumn({ name: 'user_id' })
+    user: User;
+
+    @Column({ type: 'varchar', length: 150 })
+    @Index()
+    itemName: string;
+
+    @Column({ type: 'text', nullable: true })
+    description: string;
+
+    @Column({
+        type: 'enum',
+        enum: WishlistStatus,
+        default: WishlistStatus.PENDING
+    })
+    status: WishlistStatus;
+
+
+    @Index({ spatial: true })
+    @Column({
+        type: 'geography',
+        spatialFeatureType: 'Point',
+        srid: 4326,
+        nullable: true,
+    })
+    location: any;
+}
